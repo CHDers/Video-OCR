7c82142b828a949bab40e9cf3d8d4fe1baafc73b
Yanjun_Hao <haoyanjun1996@163.com>
2024-02-05 17:01:14 +0800

    add easyocr结果

--- ocr.py
+++ ocr.py
@@ -2,13 +2,14 @@
 # -*- coding: utf-8 -*-
 # @Time    : 2024/1/25 9:24
 # @Author  : Yanjun Hao
-# @Site    : 
+# @Site    :
 # @File    : ocr.py
-# @Software: PyCharm 
+# @Software: PyCharm
 # @Comment :
 
 from PIL import Image, ImageEnhance
 import pytesseract
+import shutil
 from tqdm import tqdm
 import pandas as pd
 import cv2
@@ -19,6 +20,9 @@ from rich import print
 import re
 import sys
 from pathlib import Path
+import warnings
+
+warnings.filterwarnings("ignore")
 
 FILE = Path(__file__).resolve()
 ROOT_0 = FILE.parents[0]  # project root directory
@@ -28,18 +32,21 @@ if str(ROOT_0) not in sys.path:
 if str(ROOT_1) not in sys.path:
     sys.path.append(str(ROOT_1))  # add ROOT to PATH
 from cfg.cfg import *
-from src.pic_ocr import ocr_text_of_frame_by_pytesseract, split_video_to_frames, ocr_text_of_frame_by_paddleocr
-import warnings
-warnings.filterwarnings("ignore")
+from src.pic_ocr import (ocr_text_of_frame_by_pytesseract, split_video_to_frames, ocr_text_of_frame_by_paddleocr,
+                         ocr_text_of_frame_by_easyocr)
 
 
 def main(is_multiprocessing: bool = False) -> None:
-    video_path = FILE_ROOT / "video.mp4"
+    ocr_mode = ocr_text_of_frame_by_easyocr
+    video_path = FILE_ROOT / "1706173987235.mp4"
     frames_path = FIGURE_PATH / "Frames"
 
-    # 视频 --> 一帧一帧照片
+    # 视频 --> 单帧照片
     frame_filename_list, _ = split_video_to_frames(video_path=str(video_path), output_folder=frames_path)
     # frame_filename_list = glob.glob(str(frames_path) + "/*")
+
+    # 每隔3帧识别一次
+    frame_filename_list = frame_filename_list[::4]
 
     # 识别每祯视频的文字
     if is_multiprocessing:
@@ -47,9 +54,10 @@ def main(is_multiprocessing: bool = False) -> None:
         #  (https://www.cnblogs.com/azureology/p/13212723.html)
         #  (https://cloud.tencent.com/developer/article/1888969)
         print(f"[bold green]Multiprocessing --> cpu_count: {cpu_count()}[/bold green]")
-        with Pool(int(cpu_count() / 3)) as pool:
+        with Pool(int(cpu_count() / 5)) as pool:
             ocr_list = list(
-                tqdm(pool.imap(ocr_text_of_frame_by_pytesseract, frame_filename_list), total=len(frame_filename_list)))
+                tqdm(pool.imap(ocr_mode, frame_filename_list), total=len(frame_filename_list),
+                     desc=f"OCR", postfix=f"OCR Model", colour="green"))
             # ocr_list = pool.map(ocr_text_of_frame, frame_filename_list[::2])
 
         # thread_pool = Pool(int(cpu_count() / 2))
@@ -59,16 +67,18 @@ def main(is_multiprocessing: bool = False) -> None:
     else:
         # SECTION: 单线程
         ocr_list = []
-        for frame_filename_idx in tqdm(range(len(frame_filename_list[:10])), desc=f"OCR", postfix=f"OCR Model",
+        for frame_filename_idx in tqdm(range(len(frame_filename_list)), desc=f"OCR", postfix=f"OCR Model",
                                        colour="green"):
-            ocr_list.append(ocr_text_of_frame_by_pytesseract(frame_path=frame_filename_list[frame_filename_idx]))
+            ocr_list.append(ocr_mode(frame_path=frame_filename_list[frame_filename_idx]))
 
     ocr_df = pd.DataFrame(ocr_list)
 
     # ocr_df['timestamp'] = ocr_df['timestamp'].apply(lambda x: re.sub(r'\.0$', '', x))  # 去掉末尾的.0
     for column in ocr_df.columns:
-        if column in ['timestamp', 'pic index']:
+        if column == 'timestamp':
             ocr_df[column] = ocr_df[column].apply(lambda x: x.replace(" ", "") if x else None)
+        elif column == 'pic index':
+            ocr_df[column] = ocr_df[column].apply(lambda x: x.replace(" ", "/") if x else None)
         else:
             ocr_df[column] = ocr_df[column].apply(lambda x: x.replace('\n', '') if x else None)
 
@@ -77,13 +87,16 @@ def main(is_multiprocessing: bool = False) -> None:
 
     ocr_df.sort_values(by=['frame_path'], inplace=True, ascending=True)
 
-    ocr_df.drop_duplicates(subset=['timestamp', 'pic index'], keep='first', inplace=True)
+    ocr_df.drop_duplicates(subset=['timestamp', 'pic index', 'text'], keep='first', inplace=True)
 
     ocr_df['timestamp'] = pd.to_datetime(ocr_df['timestamp'], format='%Y-%m-%d-%H:%M:%S', errors='ignore')
     ocr_df.to_csv(RESULT_PATH / f"{video_path.stem}_ocr_result.csv", index=False, encoding='utf-8-sig')
+
+    # 清空文件夹
+    shutil.rmtree(frames_path, ignore_errors=True)
 
     print("🚀🚀🚀 [italic bold green]Code Ending")
 
 
 if __name__ == '__main__':
-    main(is_multiprocessing=False)
+    main(is_multiprocessing=True)
--- src/pic_ocr.py
+++ src/pic_ocr.py
@@ -31,8 +31,10 @@
 from cfg.cfg import *
 from PIL import Image, ImageEnhance
 import pytesseract
+import easyocr
 from paddleocr import PaddleOCR
 import shutil
+import math
 import pandas as pd
 import cv2
 import os
@@ -52,7 +54,105 @@ if str(ROOT_1) not in sys.path:
 pytesseract.pytesseract.tesseract_cmd = r'd:\SoftWare\Tesseract-OCR\tesseract.exe'
 
 
+class IdentifyText:
+    def __init__(self) -> None:
+        # 定义相似颜色的阈值，5~200之间为最佳值，5~500为有效值
+        self.threshold = 100
+        img_path = r'screen\screen.png'
+        new_img_path = r'screen\new_screen.png'
+        if self.transformedImage(img_path, new_img_path):
+            print(self.characterRecognition(new_img_path))
+
+    # 计算两个颜色之间的欧几里得距离
+    def color_distance(self, c1, c2):
+        # 如果图片没有透明通道则不需要传入和计算a通道
+        r1, g1, b1, a1 = c1
+        r2, g2, b2, a2 = c2
+        return math.sqrt((r1 - r2) ** 2 + (g1 - g2) ** 2 + (b1 - b2) ** 2 + (a1 - a2) ** 2)
+
+    # 转化图像为白底黑字，以提高识别准确性
+    def transformedImage(self, img_path, new_img_path):
+        # 打开原图
+        img = Image.open(img_path)
+        # 创建一个白色的背景图像
+        new_img = Image.new('RGBA', img.size, (255, 255, 255, 255))
+        # 遍历所有像素点
+        for x in range(img.width):
+            for y in range(img.height):
+                # 获取当前像素点的颜色
+                color = img.getpixel((x, y))
+                # 如果原图当前坐标颜色与给定颜色相似，则在背景图中相同的坐标写入黑色像素点
+                if self.color_distance(color, (247, 245, 244, 255)) < self.threshold:
+                    new_img.putpixel((x, y), (0, 0, 0, 255))
+        # 保存新图像
+        new_img.save(new_img_path)
+        return True
+
+    # 文字识别
+    def characterRecognition(self, new_img_path):
+        # 感觉好像没有必要进行灰度和二值化处理了，白底黑字的准确性挺高的，代码留这，你们自己看着整
+        # 读取新图像
+        # img = cv2.imread(r'screen\screen2.png')
+        # # 将图片转换为灰度图像
+        # gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        # # 对图像进行二值化处理
+        # thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+        # config = "--psm 7 --oem 3 -c tessedit_char_whitelist=0123456789"
+        # text = pytesseract.image_to_string(thresh, config=config)
+
+        # 读取新图像
+        img = cv2.imread(r'screen\screen2.png')
+        # 进行文字识别
+        # --psm 7 单行识别 , --oem 3 使用 LSTM OCR 引擎 , -c tessedit_char_whitelist=0123456789 只识别数字字符
+        config = "--psm 7 --oem 3 -c tessedit_char_whitelist=0123456789"
+        text = pytesseract.image_to_string(img, config=config)
+
+        # 防止识别不到报错
+        try:
+            # 去除其他符号，对数字进行重新整合
+            return int(''.join(filter(str.isdigit, text)))
+        except Exception:
+            return '未能识别文字'
+
+
+class ScreenShot:
+    def __init__(self, img_path) -> None:
+        # 定义相似颜色的阈值，5~200之间为最佳值，5~500为有效值
+        self.threshold = 200
+        self.img_path = img_path
+        self.new_img_path = img_path
+        self.transformedImage(self.img_path, self.new_img_path)
+
+    # 计算两个颜色之间的欧几里得距离
+    def color_distance(self, c1, c2):
+        # 如果图片没有透明通道则不需要传入和计算a通道
+        r1, g1, b1 = c1
+        r2, g2, b2 = c2
+        return math.sqrt((r1 - r2) ** 2 + (g1 - g2) ** 2 + (b1 - b2) ** 2)
+
+    # 转化图像为白底黑字，以提高识别准确性
+    def transformedImage(self, img_path, new_img_path):
+        # 打开原图
+        img = Image.open(img_path)
+        # 创建一个白色的背景图像
+        new_img = Image.new('RGB', img.size, (255, 255, 255))
+        # 遍历所有像素点
+        for x in range(img.width):
+            for y in range(img.height):
+                # 获取当前像素点的颜色
+                color = img.getpixel((x, y))
+                # 如果原图当前坐标颜色与给定颜色相似，则在背景图中相同的坐标写入黑色像素点
+                if self.color_distance(color, (0, 0, 0)) < self.threshold:
+                    new_img.putpixel((x, y), (0, 0, 0))
+        # 保存新图像
+        new_img.save(new_img_path)
+        return True
+
+
 def ocr_text_of_frame_by_paddleocr(frame_path: str) -> dict:
+    """
+    基于paddleocr识别文字
+    """
     ocr = PaddleOCR(lang='ch')
 
     """🐱‍👓识别超时就停止"""
@@ -98,34 +198,41 @@ def ocr_text_of_frame_by_paddleocr(frame_path: str) -> dict:
 
 
 def ocr_text_of_frame_by_pytesseract(frame_path: str) -> dict:
-    img = Image.open(frame_path)
-    width, height = img.size
+    """
+    基于pytesseract识别文字
+    """
+    # NOTE: =============================Image=================================
+    # ScreenShot(img_path=frame_path)
+    img_by_Image = Image.open(frame_path)
+    # img_by_Image = img_by_Image.convert("1")  # 图片转为黑白
+    width, height = img_by_Image.size
     # img.show()
 
     # 图片裁剪
     # 将要裁剪的图片块距原图左边界距左边距离，上边界距上边距离，右边界距左边距离，下边界距上边的距离。
     pic_idx_image_box = (1980, 0, 2140, 18)
-    pic_idx_crop_img = img.crop(pic_idx_image_box)
+    pic_idx_crop_img = img_by_Image.crop(pic_idx_image_box)
     # time_box = (238,0,395,18)
     # time_crop_img = img.crop(time_box)
     # time_crop_img.show()
 
     # 增强图片对比度
-    enhancer = ImageEnhance.Contrast(img)
+    enhancer = ImageEnhance.Contrast(img_by_Image)
     img_contrast = enhancer.enhance(2.0)
     pic_idx_enhancer = ImageEnhance.Contrast(pic_idx_crop_img)
     pic_idx_img_contrast = pic_idx_enhancer.enhance(2.0)
 
     # 指定配置，开启多列文字处理
     custom_config = r'--oem 3 --psm 6'
+    # custom_config = r"--psm 7 --oem 3 -c tessedit_char_whitelist=0123456789"
 
     """🐱‍👓识别超时就停止"""
     try:
         all_text = pytesseract.image_to_string(img_contrast,
-                                               # lang='chi_sim',
-                                               timeout=0.5,
-                                               #    config=custom_config,
-                                               )
+                                                # lang='chi_sim',
+                                                timeout=0.5,
+                                                #    config=custom_config,
+                                                )
 
         # 使用正则表达式提取日期时间信息
         time_match = re.search(r'\((.*?) UTC\)', all_text)
@@ -146,10 +253,10 @@ def ocr_text_of_frame_by_pytesseract(frame_path: str) -> dict:
             pic_idx = pic_idx_match.group(1)
         else:
             pic_idx_text = pytesseract.image_to_string(pic_idx_img_contrast,
-                                                       # lang='chi_sim',
-                                                       timeout=0.5,
-                                                       #    config=custom_config,
-                                                       )
+                                                        # lang='chi_sim',
+                                                        timeout=0.5,
+                                                        #    config=custom_config,
+                                                        )
             second_ocr_pic_idx_match = re.search(
                 r'Current Image: (.+?)', pic_idx_text)
             if second_ocr_pic_idx_match:
@@ -173,7 +280,67 @@ def ocr_text_of_frame_by_pytesseract(frame_path: str) -> dict:
         }
 
 
+def ocr_text_of_frame_by_easyocr(frame_path: str) -> dict:
+    """基于easyocr实现文字识别
+
+    Args:
+        frame_path (str): 要识别的照片路径
+
+    Returns:
+        dict: 单张图片识别后的结果
+    """
+    # https://blog.csdn.net/guokexiaohao/article/details/126317895
+    reader = easyocr.Reader(
+        ['en'])  # 注：当执行示例代码提示下载 model 时，如果下载速度慢，可以从以下分享的云盘资源下载，下载后解压到 C:\Users\${当前电脑用户}\.EasyOCR\model 目录。
+    img_by_Image = Image.open(frame_path)
+    try:
+        all_text = reader.readtext(img_by_Image, paragraph="False")
+
+        # 使用正则表达式提取日期时间信息
+        time_match = re.search(r'\((.*?) UTC\)', all_text[0][-1])
+        time_match_ = re.search(r'\((.*?) UTO\)', all_text[0][-1])
+        if time_match:
+            timestamp = time_match.group(1)
+        else:
+            if time_match_:
+                timestamp = time_match_.group(1)
+            else:
+                timestamp = None
+
+        # 使用正则表达式提取 "Current Image: 48 / 1381"
+        # pic_idx_match = re.search(r'Current Image: (\d+/\d+)', text)
+        pic_idx_match = re.search(r'\d.*\d', all_text[3][-1])
+        if pic_idx_match:
+            pic_idx = pic_idx_match.group(0)
+        else:
+            pic_idx = None
+        return {
+            "timestamp": timestamp,
+            "pic index": pic_idx,
+            "text": "<->".join([msg[-1] for msg in all_text]),
+            "frame_path": frame_path,
+        }
+    except RuntimeError as timeout_error:
+        # Tesseract processing is terminated
+        print(f"🤗🤗🤗 [italic bold red]OCR ERROR: {frame_path}")
+        return {
+            "timestamp": None,
+            "pic index": None,
+            "text": None,
+            "frame_path": frame_path,
+        }
+
+
 def split_video_to_frames(video_path: str, output_folder: Path) -> tuple:
+    """将视频切分为一帧一帧照片
+
+    Args:
+        video_path (str): 视频路径
+        output_folder (Path): 切分的一帧一帧照片的保存路径
+
+    Returns:
+        tuple: 切分为单帧照片构成的list以及视频帧率
+    """
     # 打开视频文件
     cap = cv2.VideoCapture(video_path)
 
